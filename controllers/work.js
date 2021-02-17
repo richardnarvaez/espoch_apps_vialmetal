763b50d13823f395ee99e69d814dbe85e1c7d5b0
AndresGav <andresgavino11@hotmail.com>
2021-02-17 15:40:57 -0500

    Crud Work Completed

--- controllers/work.js
+++ controllers/work.js
@@ -12,17 +12,17 @@ export async function getAllWorks() {
  }
  export async function insertWork(dataWork){
     const db = await connect()
-    const result = await db.query(`INSERT INTO users (id_responsable, id_agreement, description, creadted_at, finished_at, update_at, price_work, status)
-    VALUES( ${dataWork.id_responsable},	${dataWork.id_agreement} , ${dataWork.image})`) //ME QUEDO AQUI
+    const result = await db.query(`INSERT INTO users (id_responsable, id_agreement, description, created_at, finished_at, update_at, price_work, status)
+    VALUES( ${dataWork.id_responsable},	${dataWork.id_agreement} , ${dataWork.description}, ${dataWork.created_at}, ${dataWork.finished_at}, ${dataWork.update_at}, ${dataWork.price_work}, ${dataWork.status})`) 
     return result.recordsets
  }
  export async function updateWork(id, dataUser){
     const db = await connect()
-    const result = await db.query(`UPDATE users SET name = ${dataUser.name}, email = ${dataUser.email}, image = ${dataUser.image} WHERE id = ${id}`)
+    const result = await db.query(`UPDATE users SET description = ${dataUser.description}, created_at = ${dataUser.created_at}, finished_at = ${dataUser.finished_at}, update_at = ${dataUser.update_at}, created_at = ${dataUser.price_work}, status = ${dataUser.status} WHERE id_work = ${id}`)
     return result.recordsets
  }
  export async function deleteWork(id){
     const db = await connect()
-    const result = await db.query(`DELETE FROM users WHERE id = ${id}`)
+    const result = await db.query(`DELETE FROM work WHERE id_work = ${id}`)
     return result.recordsets
  }

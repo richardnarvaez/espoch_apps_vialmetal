49b9e5e05dd73829649a1e5e184b0f13b666fbb6
AndresGav <andresgavino11@hotmail.com>
2021-02-17 22:44:44 -0500

    View AdminWork

---
+++ pages/adminwork.js
@@ -0,0 +1,10 @@
+import Nav from '../components/nav'
+
+export default function  AdminWork(){
+    return(
+        <div>
+            <Nav/>
+            <h1>ADMIN WORK</h1>
+        </div>
+    )
+}

4d57f04cc4c6450604449fac3c5fae6b81cdcbe1
Richard Narvaez <richard.bvn997@gmail.com>
2021-02-19 07:29:21 -0500

    Udate Index

--- pages/index.js
+++ pages/index.js
@@ -11,56 +11,63 @@ export default function Home() {
             <link rel="icon" href="/favicon.ico" />
          </Head>
          <Nav />
-         <main>
-            <section id="hero" class="text-white tm-font-big tm-parallax">
-               <div class="back-black"></div>
-               <nav class="navbar navbar-expand-md tm-navbar" id="tmNav">
-                  <div class="container">
-                     <div class="tm-next">
-                        <a href="#hero" class="navbar-brand"><b>SIGN IN</b></a>
-                     </div>
-
-                     <button
-                        class="navbar-toggler"
-                        type="button"
-                        data-toggle="collapse"
-                        data-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                     >
-                        <i class="fas fa-bars navbar-toggler-icon"></i>
-                     </button>
-                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ml-auto">
-                           <li class="nav-item">
-                              <a class="nav-link tm-nav-link" href="/admin"><b>Admin</b></a>
-                           </li>
-                           <li class="nav-item">
-                              <a class="nav-link tm-nav-link" href=""><b>Usuarios</b></a>
-                           </li>
-                           <li class="nav-item">
-                              <a class="nav-link tm-nav-link" href=""><b>Formularios</b></a>
-                           </li>
-                        </ul>
-                     </div>
+         <main id="hero" class="text-white tm-font-big tm-parallax">
+            <div class="back-black"></div>
+            <nav class="navbar navbar-expand-md tm-navbar" id="tmNav">
+               <div class="container">
+                  <div class="tm-next">
+                     <a href="/" class="navbar-brand">
+                        <b>VIALMETAL</b>
+                     </a>
                   </div>
-               </nav>
 
-               <div class="text-center tm-hero-text-container">
-                  <div class="tm-hero-text-container-inner">
-                     <h2 class="tm-hero-title">VIALMETAL</h2>
-                     <p class="tm-hero-subtitle">
-                        SISTEMA DE GESTIÓN DE MATERIALES
-                  <br />Pantalla de Inicio
-               </p>
+                  <button
+                     class="navbar-toggler"
+                     type="button"
+                     data-toggle="collapse"
+                     data-target="#navbarSupportedContent"
+                     aria-controls="navbarSupportedContent"
+                     aria-expanded="false"
+                     aria-label="Toggle navigation"
+                  >
+                     <i class="fas fa-bars navbar-toggler-icon"></i>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                     <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                           <a class="nav-link tm-nav-link" href="/admin">
+                              <b>Admin</b>
+                           </a>
+                        </li>
+                        {/* <li class="nav-item">
+                           <a class="nav-link tm-nav-link" href="">
+                              <b>Usuarios</b>
+                           </a>
+                        </li> */}
+                        <li class="nav-item">
+                           <a class="nav-link tm-nav-link" href="auth/login">
+                              <b>Iniciar sesión</b>
+                           </a>
+                        </li>
+                     </ul>
                   </div>
                </div>
+            </nav>
 
-               <footer class="text-center small tm-footer">
-                  <p class="mb-0">Copyright &copy; 2021 VIALMETAL</p>
-               </footer>
-            </section>
+            <div class="text-center tm-hero-text-container">
+               <div class="tm-hero-text-container-inner">
+                  <h2 class="tm-hero-title">VIALMETAL</h2>
+                  <p class="tm-hero-subtitle">
+                     SISTEMA DE GESTIÓN DE MATERIALES
+                     <br />
+                     Pantalla de Inicio
+                  </p>
+               </div>
+            </div>
+
+            <footer class="text-center small tm-footer">
+               <p class="mb-0">Copyright &copy; 2021 VIALMETAL</p>
+            </footer>
          </main>
       </>
    )

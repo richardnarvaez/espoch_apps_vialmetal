7b3203bb5ad064e044aaff87637df9b6000d2eaf
AndresGav <andresgavino11@hotmail.com>
2021-02-20 17:12:50 -0500

    Desing View AdminWork V3.0

--- components/card_work.js
+++ components/card_work.js
@@ -3,16 +3,27 @@ export default function Card({ data, onClick }) {
    return (
       <>
          <div
-            class="col-sm-4 item-work"
+            class="col-sm-3"
             onClick={() => {
                console.log(i)
                i++
                onClick('Naaaada')
             }}
          >
-            <h5 class="card-title">{data.name}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">{data.email}</h6>
-            <img src={data.image} />
+
+            <div className="item-work">
+
+               <div className="img-transport">
+                  <img src={data.image} />
+               </div>
+               
+               <div className="details-transport">
+                  <h5 className="trasnport-name">{data.name}</h5>
+                  <h5 className="trasnport-status" >{data.name}</h5>
+               </div>
+               
+            </div>
+            
          </div>
       </>
    )
--- layouts/adminwork_layout.js
+++ layouts/adminwork_layout.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import CardW from '../components/card_work'
-import Nav from '../components/nav'
+
 
 export default function Admin() {
    const [users, setUsers] = useState()
@@ -22,92 +22,103 @@ export default function Admin() {
    return (
       <>
          <div className="body-adminwork">
+
+            <h1 className="title-page">Nueva Obra</h1>
+
             <div className="content-newwork">
+               
                <div className="new-work">
-                  <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                     <li class="nav-item">
-                        <a
-                           class="nav-link active"
-                           id="pills-home-tab"
-                           data-toggle="pill"
-                           href="#pills-home"
-                           role="tab"
-                           aria-controls="pills-home"
-                           aria-selected="true"
+                  <h2>Material de trabajo</h2>
+                  
+                  <div className="work-options">
+                     <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                        <li class="nav-item">
+                           <a
+                              class="nav-link active"
+                              id="pills-home-tab"
+                              data-toggle="pill"
+                              href="#pills-home"
+                              role="tab"
+                              aria-controls="pills-home"
+                              aria-selected="true"
+                           >
+                              Transporte
+                           </a>
+                        </li>
+                        <li class="nav-item">
+                           <a
+                              class="nav-link"
+                              id="pills-profile-tab"
+                              data-toggle="pill"
+                              href="#pills-profile"
+                              role="tab"
+                              aria-controls="pills-profile"
+                              aria-selected="false"
+                           >
+                              Herramientas
+                           </a>
+                        </li>
+                        <li class="nav-item">
+                           <a
+                              class="nav-link"
+                              id="pills-contact-tab"
+                              data-toggle="pill"
+                              href="#pills-contact"
+                              role="tab"
+                              aria-controls="pills-contact"
+                              aria-selected="false"
+                           >
+                              Detalles
+                           </a>
+                        </li>
+                     </ul>
+                     <div class="tab-content" id="pills-tabContent">
+                        <div
+                           class="tab-pane fade show active"
+                           id="pills-home"
+                           role="tabpanel"
+                           aria-labelledby="pills-home-tab"
                         >
-                           Informacion
-                        </a>
-                     </li>
-                     <li class="nav-item">
-                        <a
-                           class="nav-link"
-                           id="pills-profile-tab"
-                           data-toggle="pill"
-                           href="#pills-profile"
-                           role="tab"
-                           aria-controls="pills-profile"
-                           aria-selected="false"
+                           <div class="row">
+                              {!users ? (
+                                 <>
+                                    <div class="spinner-border" role="status">
+                                       <span class="sr-only">Loading...</span>
+                                    </div>
+                                 </>
+                              ) : (
+                                 
+                                 users.map((item, i) => {
+                                    return (
+                                       <>
+                                          <CardW data={item} onClick={setListado} />
+                                       </>
+                                    )
+                                 })
+                              )}
+                           </div>
+                        </div>
+                        <div
+                           class="tab-pane fade"
+                           id="pills-profile"
+                           role="tabpanel"
+                           aria-labelledby="pills-profile-tab"
                         >
-                           Herramientas
-                        </a>
-                     </li>
-                     <li class="nav-item">
-                        <a
-                           class="nav-link"
-                           id="pills-contact-tab"
-                           data-toggle="pill"
-                           href="#pills-contact"
-                           role="tab"
-                           aria-controls="pills-contact"
-                           aria-selected="false"
+                           ...
+                        </div>
+                        <div
+                           class="tab-pane fade"
+                           id="pills-contact"
+                           role="tabpanel"
+                           aria-labelledby="pills-contact-tab"
                         >
-                           Vehiculos
-                        </a>
-                     </li>
-                  </ul>
-                  <div class="tab-content" id="pills-tabContent">
-                     <div
-                        class="tab-pane fade show active"
-                        id="pills-home"
-                        role="tabpanel"
-                        aria-labelledby="pills-home-tab"
-                     >
-                        <div class="row">
-                           {!users ? (
-                              <>
-                                 <div class="spinner-border" role="status">
-                                    <span class="sr-only">Loading...</span>
-                                 </div>
-                              </>
-                           ) : (
-                              users.map((item, i) => {
-                                 return (
-                                    <>
-                                       <CardW data={item} onClick={setListado} />
-                                    </>
-                                 )
-                              })
-                           )}
+                           ...
                         </div>
                      </div>
-                     <div
-                        class="tab-pane fade"
-                        id="pills-profile"
-                        role="tabpanel"
-                        aria-labelledby="pills-profile-tab"
-                     >
-                        ...
-                     </div>
-                     <div
-                        class="tab-pane fade"
-                        id="pills-contact"
-                        role="tabpanel"
-                        aria-labelledby="pills-contact-tab"
-                     >
-                        ...
-                     </div>
                   </div>
+                  
                </div>
+
                <div className="list-work">
                   <h2>Lista</h2>
                   <div className="content-list">
@@ -132,6 +143,7 @@ export default function Admin() {
                            })
                         )}
                      </div>
+
                      <div className="item-list">
                         <h5>Material</h5>
                         <div style={{ borderBottom: 'solid 0.5px #ececec', padding: 8 }}>
@@ -153,6 +165,7 @@ export default function Admin() {
                            <p>12$</p>
                         </div>
                      </div>
+
                      <div className="item-list">
                         <h5>Pintura</h5>
                         <div style={{ borderBottom: 'solid 0.5px #ececec', padding: 8 }}>
@@ -178,13 +191,19 @@ export default function Admin() {
                         </div>
                      </div>
                   </div>
+
                   <div className="btn-agregar">
-                     <button class="btn btn-primary" type="button">
-                        Agregar
+                     <button class="" type="button">
+                        Crear Obra
                      </button>
                   </div>
+                  
                </div>
             </div>
+         </div>
+
+         <div className="newwork-footer">
+
          </div>
       </>
    )

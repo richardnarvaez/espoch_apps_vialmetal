8d5c3d245e651660ba965c4f3b2372214b21064c
Richard Narvaez <richard.bvn997@gmail.com>
2021-02-22 17:45:26 -0500

    Udate ADMIN DATOS LISTOS DE OBRAS

--- layouts/admin_fragment_1.js
+++ layouts/admin_fragment_1.js
@@ -4,20 +4,21 @@ import Link from 'next/link'
 import Card from '../components/card'
 
 export default function AdminF1() {
-
    // VARIABLES "ESTADO"
    const [list_obras, setListObras] = useState([])
+   const [error, setError] = useState(false)
 
-   // FETCH DATOS DE LA API sdfsdf
+   // FETCH DATOS DE LA API
    // LOS DATOS PASAN A LA VARIABLE list_obras como LISTA []
    useEffect(() => {
       fetch('/api/data/work/active')
-         .then(res => res.json())
-         .then(result =>
-            setListObras(result))
-         .catch(e => {
-            console.log("ERRPR: >>>>", e)
-            setListObras([{}])
+         .then((res) => res.json())
+         .then((result) => {
+            setListObras(result)
+         })
+         .catch((e) => {
+            console.log('ERRPR: >>>>', e)
+            setError(true)
          })
    }, [])
 
@@ -26,18 +27,19 @@ export default function AdminF1() {
          <a className="bt-new-work" href="/adminwork">
             <h5>Nueva Obra</h5>
          </a>
-
-         <div className="div" className="row">
-            {
-               !list_obras ? (<>CARGANDO DATO...</>) :
+         {error ? (
+            <>Error de conexion</>
+         ) : (
+            <div className="div" className="row">
+               {!list_obras ? (
+                  <>CARGANDO DATO...</>
+               ) : (
                   list_obras.map((item, i) => {
-                     return (
-                        <Card key={i} data={item} href={"/details/" + item.id_work} />
-                     )
+                     return <Card key={i} data={item} href={'/details/' + item.id_work} />
                   })
-            }
-         </div>
-
+               )}
+            </div>
+         )}
       </>
    )
 }
